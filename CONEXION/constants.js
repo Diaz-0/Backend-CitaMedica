a345ad169a314ead84042c3c0fe98f30c4890226
Jesús Díaz <diazmontejo125@gmail.com>
2025-05-23 11:15:44 -0600

    LISTO3

--- CONEXION/constants.js
+++ CONEXION/constants.js
@@ -1,11 +1,11 @@
 const constants = {
     DB_USER: "w3aqg87icfhnkt1i",
-    DB_PASSWORD: "3459",
+    DB_PASSWORD: "te9oaq3ketys9mnh",
     DB_HOST: "olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     DB_NAME: "u4vih1vlxpiw5j5y",
     DB_PORT: 	3306,
     API_VERSION: "V1",
-    IP_SERVER: "	olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    IP_SERVER: "olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     JWT_SECRET: "3459",
   };
   
